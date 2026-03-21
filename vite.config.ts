acfb2847987fd535a400b1461bbbe4974d8bdfe7
rahilmuliashia94-create <rahilmuliashia94@gmail.com>
2026-03-21 19:47:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/everest-security-website/',
+  base: '/everest-technologies/',
   plugins: [react()],
   resolve: {
     alias: {
